7c5e8158d57ba98ea18cbf177a7ab6ecc4f971f1
Atique Akhtar <atiqueakhtar786@gmail.com>
2021-09-26 15:31:06 +0530

    feat: added logic and error handling

---
+++ script.js
@@ -0,0 +1,48 @@
+let billAmount = document.querySelector("#bill-amount");
+let cashAmount = document.querySelector("#cash-amount");
+let button = document.querySelector("button");
+let billError = document.querySelector("#bill-error");
+let cashError = document.querySelector("#cash-error");
+let noOfNotesList = document.querySelectorAll(".no-of-notes");
+
+let denominations = [2000, 500, 100, 20, 10, 5, 1]
+function billErroMessage(message)
+{
+    billError.style.display="block";
+    billError.innerText = message;
+}
+function cashEerroMessage(message)
+{
+    cashError.style.display="block";
+    cashError.innerText = message;
+}
+button.addEventListener("click", () => {
+    if(billAmount.value>0)
+    {
+        billError.style.display="none";
+        if(cashAmount.value>0)
+        {
+            cashError.style.display="none";
+            if(cashAmount.value >= billAmount.value)
+            {
+                cashError.style.display="none";
+                let difference = cashAmount.value - billAmount.value;
+                for(let i=0; i<denominations.length; i++)
+                {
+                    let noOfNotes = Math.floor(difference/denominations[i]);
+                    difference = difference%denominations[i];
+                    noOfNotesList[i].innerText = noOfNotes;
+                }
+            }
+            else{
+                cashEerroMessage("Cash Amount should be more that or equal to the Bill Amount");
+            }
+        }
+        else{
+            cashEerroMessage("Cash Amount should to be more that 0");
+        }
+    }
+    else{
+        billErroMessage("Bill Amount should to be more that 0");
+    }
+});
